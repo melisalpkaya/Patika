f3094ab7c676c57d00fb71f66f43df904cd50d16
Melis Alpkaya <melisalpkaya200011@gmail.com>
2022-11-06 22:52:27 +0300

    Add files via upload

---
+++ Java/VucutKitleIndex.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VucutKitleIndex {
+    public static void main(String[] args) {
+        int kilo;
+        double boy, indeks;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz :");
+        boy = input.nextDouble();
+        System.out.println(" Lütfen kilonuzu giriniz :");
+        kilo=input.nextInt();
+
+        indeks = kilo / (boy * boy);
+        System.out.println("Vücut Kitle İndeksiniz : "+ indeks);
+
+    }
+}
