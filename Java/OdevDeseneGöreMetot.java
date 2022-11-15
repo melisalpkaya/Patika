fa7c33f4e53a1dc522e187c581373d34cb4af577
Melis Alpkaya <melisalpkaya200011@gmail.com>
2022-11-15 18:33:06 +0300

    Update OdevDeseneGöreMetot.java

--- Java/OdevDeseneGöreMetot.java
+++ Java/OdevDeseneGöreMetot.java
@@ -24,6 +24,7 @@ public class OdevDeseneGöreMetot {
     }
 
     public static void main(String[] args) {
+         //Girilen sayı 0 veya negatif olduğu yere kadar girilen sayıdan 5 rakamını çıkarın. Her çıkarma işlemi sırasında ekrana son değeri yazdırın. Sayı negatif veya 0 olduktan sonra tekrar 5 ekleyin. Yine her ekleme işleminde sayının son değerini ekrana yazdırın.
 
         int number, result, tempNumber;
         Scanner inp = new Scanner(System.in);
